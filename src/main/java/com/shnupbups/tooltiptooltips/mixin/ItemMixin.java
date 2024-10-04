dd24fa87549a2ba022aefd7d7466ec8021821c0d
JFronny <git@akbvopenfl1.hopto.org>
2024-10-04 12:29:49 +0200

    1.21

--- src/main/java/com/shnupbups/tooltiptooltips/mixin/ItemMixin.java
+++ src/main/java/com/shnupbups/tooltiptooltips/mixin/ItemMixin.java
@@ -1,45 +1,41 @@
 package com.shnupbups.tooltiptooltips.mixin;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.item.TooltipContext;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.MiningToolItem;
-import net.minecraft.item.ToolItem;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.*;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.world.World;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(Item.class)
 public abstract class ItemMixin {
-	@Shadow
-	public abstract FoodComponent getFoodComponent();
-
-	@Inject(method = "appendTooltip(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Ljava/util/List;Lnet/minecraft/client/item/TooltipContext;)V", at = @At("HEAD"))
-	public void appendTooltipInject(ItemStack stack, World world, List<Text> tooltip, TooltipContext context, CallbackInfo ci) {
+	@Inject(method = "appendTooltip(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/List;Lnet/minecraft/item/tooltip/TooltipType;)V", at = @At("HEAD"))
+	public void appendTooltipInject(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type, CallbackInfo ci) {
 		List<Text> texts = new ArrayList<>();
 
 		if ((Object) this instanceof ToolItem tool) {
 			ToolMaterial material = tool.getMaterial();
 			if (tool instanceof MiningToolItem) {
-				texts.add(Text.translatable("tooltip.harvest_level", material.getMiningLevel()).formatted(Formatting.GRAY));
-				int efficiency = EnchantmentHelper.get(stack).getOrDefault(Enchantments.EFFICIENCY, 0);
+                // Level is not a thing anymore
+                // Something similar could be reconstructed from tool.getMaterial().getInverseTag(), but that wouldn't be accurate or particularly useful
+//				texts.add(Text.translatable("tooltip.harvest_level", material.getMiningLevel()).formatted(Formatting.GRAY));
+				int efficiency = context.getRegistryLookup().createRegistryLookup()
+                        .getOptional(RegistryKeys.ENCHANTMENT)
+                        .flatMap(s -> s.getOptional(Enchantments.EFFICIENCY))
+                        .map(s -> EnchantmentHelper.getEnchantments(stack).getLevel(s))
+                        .orElse(0);
 				int efficiencyModifier = efficiency > 0 ? (efficiency * efficiency) + 1 : 0;
 				MutableText speedText = Text.translatable("tooltip.harvest_speed", material.getMiningSpeedMultiplier() + efficiencyModifier).formatted(Formatting.GRAY);
 				if (efficiency > 0) {
@@ -49,21 +45,20 @@ public abstract class ItemMixin {
 			}
 			texts.add(Text.translatable("tooltip.enchantability", material.getEnchantability()).formatted(Formatting.GRAY));
 		} else if ((Object) this instanceof ArmorItem armor) {
-			ArmorMaterial material = armor.getMaterial();
-			texts.add(Text.translatable("tooltip.enchantability", material.getEnchantability()).formatted(Formatting.GRAY));
+			ArmorMaterial material = armor.getMaterial().value();
+			texts.add(Text.translatable("tooltip.enchantability", material.enchantability()).formatted(Formatting.GRAY));
 		}
 
-		if (stack.isDamageable() && (!stack.isDamaged() || !context.isAdvanced())) {
+		if (stack.isDamageable() && !stack.isDamaged()) {
 			texts.add(Text.translatable("tooltip.durability", stack.getMaxDamage() - stack.getDamage(), stack.getMaxDamage()).formatted(Formatting.GRAY));
 		}
 
-		if (stack.isFood()) {
-			FoodComponent foodComponent = this.getFoodComponent();
-			texts.add(Text.translatable("tooltip.hunger", foodComponent.getHunger()).formatted(Formatting.GRAY));
-			texts.add(Text.translatable("tooltip.saturation", foodComponent.getSaturationModifier()).formatted(Formatting.GRAY));
+		if (stack.get(DataComponentTypes.FOOD) instanceof FoodComponent foodComponent) {
+			texts.add(Text.translatable("tooltip.hunger", foodComponent.nutrition()).formatted(Formatting.GRAY));
+			texts.add(Text.translatable("tooltip.saturation", foodComponent.saturation()).formatted(Formatting.GRAY));
 		}
 
-		if (texts.size() == 1 || Screen.hasShiftDown() || context.isAdvanced()) {
+		if (texts.size() == 1 || Screen.hasShiftDown()) {
 			tooltip.addAll(texts);
 		} else if (!texts.isEmpty()) {
 			tooltip.add(Text.translatable("tooltip.press_shift").formatted(Formatting.GRAY));
